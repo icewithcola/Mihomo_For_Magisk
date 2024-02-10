992c288d08c490266bff27b0ca24e2da29a8ca00
icewithcola <me@kagurach.uk>
2024-02-10 23:03:46 +0800

    Fix bugs

--- README.md
+++ README.md
@@ -1,19 +1,25 @@
 # Mihomo For Magisk
 ## 特性
 - 自助打包更新Mihomo
-- 可预下载`原神`资源包
+- 可预下载`原神`资源包(GeoX,MMDB,Dashboard)
+- 可以自带下载附加资源(providers)
 - 更新资源带有验证逻辑防止更坏掉
 ## 打包方式
+在linux下
 `$ ./pack.sh -a`
+（需要
 ## 自定义部分
 ### files.config
-存储打包时的变量  
+存储打包时的变量，具体请看该文件中的注释  
 #### 注意
 更新`files.config`中的链接时请同时更新`./clash/clash.config`中的相同部分，*(可忽略没有的变量)*。  
-当且仅当自用时才填上您的订阅链接，**否则您的订阅也会被打包**  
+当且仅当自用时才填上您的订阅链接，**否则您的订阅也会被打包**，分发这样的打包可能会造成您的原神UID泄漏  
+### clash/template
+此处是您的模板，即是最终的config除了您template外的部分
 ## 功能
 ### 开启
-`/data/clash/scripts/clash.service -s`
+`/data/clash/scripts/clash.service -s`  
+dashboard通过[http://127.0.0.1:9090/ui](http://127.0.0.1:9090/ui)打开
 ### 关闭
 `/data/clash/scripts/clash.service -k`
 ### 更新订阅
--- pack.sh
+++ pack.sh
@@ -5,23 +5,13 @@ set -e
 . ./files.config
 
 
-# 检测环境
-if [ -z "$(wget --version)" ]; then 
-    apt install -y wget
-fi
-
-if [ -z "$(gunzip --version)" ]; then
-    apt install -y gzip
-fi
-
-
 # 下载文件
 download_binaries(){
     # mihomo
     echo "正在下载mihomo..."
-    mihomo_version=$(curl -sL "${Mihomo_url}version.txt")
+    mihomo_version=$(curl -sL "${Corefile_mihomo}version.txt")
     echo "mihomo版本: ${mihomo_version}"
-    wget "${Mihomo_url}mihomo-android-arm64-${mihomo_version}.gz" -O mihomo.gz 
+    wget "${Corefile_mihomo}mihomo-android-arm64-${mihomo_version}.gz" -O mihomo.gz 
     gunzip mihomo.gz
     mv "mihomo" ./binary/clash
     chmod 0755 ./binary/clash
@@ -29,7 +19,7 @@ download_binaries(){
 
     # curl
     echo "正在下载curl..."
-    wget "${Curl_url}" -O curl.tar.xz 
+    wget "${Corefile_curl}" -O curl.tar.xz 
     tar -xvf curl.tar.xz
     mv curl ./binary/curl
     chmod 0755 ./binary/curl
@@ -52,14 +42,49 @@ download_geoX(){
 
 
 download_config(){
-    # config.yaml
     if [ -z "${Subcript_url}" ]; then
         echo "订阅链接为空，跳过"
+    else
+        echo "正在下载config.yaml..."
+        wget "${Subcript_url}" -O ./clash/config.yaml 
+    fi
+
+    providers=$(grep -A 2 "type: http" ./clash/config.yaml)
+    if [[ -n "$providers" ]]; then
+        IFS="--"
+        declare -a provider_list=( $providers )
+        for (( i=0 ; i < ${#provider_list[@]}; i++ )); do
+            provider=${provider_list[$i]}
+            if [[ -n "$provider" ]]; then
+                url=$(echo "$provider" | grep -o 'url: "[^"]*"' | cut -d'"' -f2)
+                path=$(echo "$provider" | grep -o 'path: [^ ]*' | cut -d' ' -f2-)
+                wget $url -O "./clash/$path"
+                echo "成功下载$path"
+            fi
+        done
+    else
+        echo "没有需要下载的providers"
+    fi
+}
+
+download_dashboard(){
+    if [ -z "${Corefile_dashboard}" ]; then
+        echo "Dashboard链接为空，跳过"
         return
     fi
-    echo "正在下载config.yaml..."
-    wget "${Subcript_url}" -O ./clash/config.yaml 
+
+    if [ -d ./clash-dashboard ]; then
+        rm -rf ./clash-dashboard
+    fi
+
+    echo "正在下载dashboard..."
+    wget "${Corefile_dashboard}" -O ./clash-dashboard.zip 
+    unzip -o ./clash-dashboard.zip -d ./clash-dashboard
+    mv ./clash-dashboard/Yacd-meta-gh-pages ./clash-dashboard/dist
+    rm -rf ./clash-dashboard/yacd-gh-pages
+    rm -f ./clash-dashboard.zip
 }
+
 
 pack(){
     echo "开始打包..."
@@ -76,16 +101,20 @@ pack(){
     echo "打包完成"
 }
 
-while getopts "abgcp" opt; do
+while getopts "abdcgp" opt; do
     case $opt in
         a)
             download_binaries
             download_geoX
             download_config
+            download_dashboard
             pack
             ;;
         b)
             download_binaries
+            ;;
+        d)
+            download_dashboard
             ;;
         g)
             download_geoX
